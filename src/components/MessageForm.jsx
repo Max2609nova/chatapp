f9f21ffc7fb6644e5e823d598533a809622e26d5
Mohit Kumar <mnova2327@gmail.com>
2026-02-12 22:27:24 +0530

    Update MessageForm.jsx

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -34,4 +34,26 @@ const MessageForm = (props) => {
         className="message-input"
         placeholder="Send a message..."
         value={value}
-        
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+      <label htmlFor="upload-button">
+        <span className="image-button">
+          <PictureOutlined className="picture-icon" />
+        </span>
+      </label>
+      <input
+        type="file"
+        multiple={false}
+        id="upload-button"
+        style={{ display: 'none' }}
+        onChange={handleUpload.bind(this)}
+      />
+      <button type="submit" className="send-button">
+        <SendOutlined className="send-icon" />
+      </button>
+    </form>
+  );
+};
+
+export default MessageForm;
